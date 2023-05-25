4db84e630821e96d6b65cf3540e684b3725be920
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-25 09:53:45 -0700

    feat: add migration script

--- src/db_migration.ts
+++ src/db_migration.ts
@@ -1,44 +1,27 @@
 import mongoose from 'mongoose'
-
-interface ClueHint {
-    clue: string,
-    source: string,
-    year?: string
-}
+import { type UpdateOneModel } from 'mongodb'
+import fs from 'fs'
+import csv from 'csv-parser'
+import path from 'path'
 
 interface WordHint {
     word: string,
-    clues: Array<ClueHint>
-}
-
-const clueHintSchema = new mongoose.Schema<ClueHint>({
-    clue: {
-        type: String,
-        required: true
-    },
-    source: {
-        type: String,
-        required: true
-    },
-    year: {
-        type: String,
-        validate: {
-            validator: (v: string) => {
-                return /\d{4}/.test(v)
-            },
-            message: (props) => `${props.value} is not a valid year`
-        },
-        required: false
-    }
-})
+    clues: string[]
+} // TODO: add sources to clues
 
 const wordHintSchema = new mongoose.Schema<WordHint>({
     word: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: (v: string) => {
+                return /^[A-Z]+$/.test(v)
+            },
+            message: (props) => `Word must be all capital letters (${props.value})`
+        }
     },
     clues: {
-        type: [clueHintSchema],
+        type: [String],
         required: true
     }
 })
@@ -47,18 +30,30 @@ const WordHintModel = mongoose.model<WordHint>('WordHint', wordHintSchema)
 
 main().then(() => console.log('connected')).catch(console.log)
 
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test')
-
-    const newWordHint = new WordHintModel({
-        word: 'ABA',
-        clues: [
-            {
-                clue: 'Litigator\'s group',
-                source: 'atc',
-                year: '1997'
+function migrateToDb() {
+    const operations: mongoose.mongo.AnyBulkWriteOperation[] = []
+    fs.createReadStream(path.join(__dirname, '../clues.tsv'))
+        .pipe(csv({ separator: '\t' }))
+        .on('data', (data: any) => {
+            operations.push({
+                updateOne: {
+                    filter: { word: data.answer },
+                    update: { $addToSet: { clues: data.clue } },
+                    upsert: true
+                }
+            })
+            
+            if (operations.length % 1000 === 0) {
+                WordHintModel.collection.bulkWrite(operations, { ordered: true }).catch(() => {})
+                operations.splice(0, operations.length)
             }
-        ]
-    })
-    await newWordHint.save()
+        })
+        .on('close', () => {
+            console.log('closed stream')
+        })
+}
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/crosswordhelper')
+    migrateToDb()
 }
