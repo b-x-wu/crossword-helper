10013477d54e04562275f80230ecdb4e260fba40
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-25 12:08:36 -0700

    feat: add get word_hint endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express'
+import express, { Request } from 'express'
 import { MongooseConnection, WordHintModel } from './db'
 
 const app = express()
 
-app.get('/word_hint', (req, res) => {
-    WordHintModel.find({ word: 'ADA' }, {}).then((val) => {
+app.get('/word_hint', (req: Request<{}, any, any, { word: string }>, res) => {
+    const wordMatch: RegExp = RegExp(`^${req.query['word'].replace('_', '\\w')}$`)
+    WordHintModel.find({ word: { $regex: wordMatch } }).then((val) => {
         res.json(val)
     })
 })
@@ -12,4 +13,5 @@ app.get('/word_hint', (req, res) => {
 app.listen('3000', async () => {
     console.log('Listening on port 3000')
     await MongooseConnection
+    console.log('Connected to db')
 })
