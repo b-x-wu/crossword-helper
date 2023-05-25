6f5b0007564fea11ebfb4d96599eb16219563dfc
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-25 13:05:07 -0700

    feat: add clue_hint endpoint

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,19 @@ import { MongooseConnection, WordHintModel } from './db'
 const app = express()
 
 app.get('/word_hint', (req: Request<{}, any, any, { word: string }>, res) => {
-    const wordMatch: RegExp = RegExp(`^${req.query['word'].replace('_', '\\w')}$`)
+    const wordMatch: RegExp = RegExp(`^${req.query.word.replace('_', '\\w')}$`)
     WordHintModel.find({ word: { $regex: wordMatch } }).then((val) => {
         res.json(val)
+    })
+})
+
+app.get('/clue_hint', (req: Request<{}, any, any, { word: string }>, res) => {
+    WordHintModel.find({ word: req.query.word }).then((val) => {
+        if (val.length == 0) {
+            res.status(404).json({ message: `No clues associated with word. (${req.query.word})` })
+            return
+        }
+        res.json(val[0].clues)
     })
 })
 
